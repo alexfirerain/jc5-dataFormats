4937e1c1a3e2ab392fd1623fe07643c3231e699f
Alexander Lukyanov <lux-nova@mail.ru>
2021-06-13 21:43:50 +0300

    1st task set up, parseCSV designed

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -6,38 +6,46 @@ import com.opencsv.exceptions.CsvException;
 
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
 public class Main {
     static String[] columnMapping = {"id", "firstName", "lastName", "country", "age"};
     static String source = "data.csv";
-    static ColumnPositionMappingStrategy<Employee> fieldSet = new ColumnPositionMappingStrategy<>();
-    static {
-        fieldSet.setType(Employee. class);
-        fieldSet.setColumnMapping(columnMapping);
-    }
 
     public static void main(String[] args) {
 
+        List<Employee> list = parseCSV(columnMapping, source);
+        list.forEach(System.out::println);
+
+        String json = listToJson(list);
+        System.out.println(json);
+        writeString(json);
+
+    }
+
+    private static List<Employee> parseCSV(String[] columnMapping, String source) {
+        List<Employee> list = new ArrayList<>();
+        ColumnPositionMappingStrategy<Employee> fieldSet = new ColumnPositionMappingStrategy<>();
+        fieldSet.setType(Employee. class);
+        fieldSet.setColumnMapping(columnMapping);
 
         try (CSVReader reader = new CSVReader(new FileReader(source))){
-//            List<String[]> lines = reader.readAll();
-//            for (String[] row : lines)
-//                System.out.println(Arrays.toString(row));
-
             CsvToBean<Employee> csvGetter = new CsvToBeanBuilder<Employee>(reader)
                     .withMappingStrategy(fieldSet).build();
-
-            List<Employee> employees = csvGetter.parse();
-            employees.forEach(System.out::println);
-
-
+            list = csvGetter.parse();
         } catch (IOException e) {
             e.printStackTrace();
         }
+        return list;
+    }
 
+    private static String listToJson(List<Employee> list) {
+        return null;
+    }
 
+    private static void writeString(String jsonString) {
 
     }
 }
